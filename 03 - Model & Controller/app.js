6399bd5c3b5f9e044430f90e5ede08d0a1efee72
Oystein Hodne <oystein.hodne@gmail.com>
2014-06-23 12:42:56 +0200

    01 - 04

--- 03 - Model & Controller/app.js
+++ 03 - Model & Controller/app.js
@@ -1,5 +1,12 @@
-var app = angular.module('controllerDemo', []);
+var app = angular.module('githubViewer', []);
 
-app.controller('MainController', function($scope){
-    $scope.message = 'Hello Angular from controller!';
+app.controller('MainController', function($scope, $http){
+    $scope.username = 'angular';
+
+    $scope.searchUser = function(username){
+        $http.get('https://api.github.com/users/' + username)
+            .then(function(response){
+                $scope.user = response.data;
+            })
+    }
 });
