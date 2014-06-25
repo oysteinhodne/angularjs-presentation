bcab331aadbc3b54d43e85417651c38a5fba97da
Oystein Hodne <oystein.hodne@gmail.com>
2014-06-25 10:53:35 +0200

    Dependency injection + modifications

--- 06 - Dependency Injection/app.js
+++ 06 - Dependency Injection/app.js
@@ -1,1 +1,12 @@
-var app = angular.module('bootstrapping', []);
+var app = angular.module('dependencyInjection', ['otherModule']);
+
+app.value('numberValue', 999);
+
+app.controller('Controller', function($scope, numberValue, otherModuleValue){
+    $scope.numberValue = numberValue;
+    $scope.otherModuleValue = otherModuleValue;
+});
+
+var otherModule = angular.module('otherModule', []);
+
+otherModule.value('otherModuleValue', 'This is a value from the other module');
